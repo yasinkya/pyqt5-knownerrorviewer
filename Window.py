732116624d31251c1c15897d5c090cb3a9f4ac12
Yasin KAYA <yasinkaya1007@gmail.com>
2021-08-15 20:10:37 +0300

    error fix

--- Window.py
+++ Window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QGridLayout
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget
 import uis.mainUI
 import functions
 import jsonPy_global
@@ -12,34 +12,32 @@ class New(QMainWindow):
         # self.setGeometry(x, y, w, h)
         self.setWindowTitle("Main")
         self.mainUI = QMainWindow()
-        self.up_tabwdget = QWidget()
         self.init_ui()
 
     def init_ui(self):
         # set mainui to window
         self.mainUI = uis.mainUI.UiMainWindow()
         self.mainUI.setup_ui(self)
-
         # tabWidget define
-        self.up_tabwdget = self.mainUI.tabWidget
+        self.tabwid_main = self.mainUI.tabWidget
         # sync qtabwidget
         self.sync_tabs(jsonPy_global.jsondata)
         # tabbar clicked- idx current index of clicked tab
-        self.up_tabwdget.currentChanged.connect(lambda idx: self.tab_changed(jsonPy_global.jsondata["feeds"][idx], idx))
+        self.tabwid_main.currentChanged.connect(lambda idx: self.tab_changed(jsonPy_global.jsondata["feeds"][idx], idx))
         self.tab_changed(jsonPy_global.jsondata["feeds"][0], 0)
 
     # Add tabs and change the their text's through json
     def sync_tabs(self, data):
         for i in data["feeds"]:
-            tab = QWidget()
-            self.up_tabwdget.addTab(tab, str(i["id"]))
+            tab_main = QWidget()
+            self.tabwid_main.addTab(tab_main, str(i["id"]))
 
     # clicked tabbar
     def tab_changed(self, data, currenttab_idx):
-        tablay = QVBoxLayout()
+        tablay = QVBoxLayout(self.tabwid_main.find(currenttab_idx))
         # TODO: when u clicked a tab, its create a new down tab but whats happening the old tab
         functions.newtab(tablay, data.keys(), currenttab_idx)
 
         # do not set defined layout to the current clicked tab again
-        if not self.up_tabwdget.currentWidget().layout():
-            self.up_tabwdget.currentWidget().setLayout(tablay)
+        if not self.tabwid_main.currentWidget().layout():
+            self.tabwid_main.currentWidget().setLayout(tablay)
