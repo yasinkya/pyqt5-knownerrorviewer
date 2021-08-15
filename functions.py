df0468d3e004f24d7d7c30b75097ed64d1058b00
Yasin KAYA <yasinkaya1007@gmail.com>
2021-08-15 22:00:46 +0300

    jsondaki bir datanın elemanı dict veya listse

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QTabWidget, QVBoxLayout, QWidget, QLabel, QSizePolicy, QGridLayout, QPlainTextEdit
+from PyQt5.QtWidgets import QTabWidget, QVBoxLayout, QWidget, QGridLayout, QPlainTextEdit
+from PyQt5 import QtGui, QtCore
 import json
 import jsonPy_global
 
@@ -10,80 +11,39 @@ def read_json():
     return data
 
 
-def newtab(layout: QVBoxLayout, tab_main: QWidget, keys: set, currenttab_idx):
-    # define a new tabwidget then add this to up tabwidget's layout
+def newtab(layout: QVBoxLayout, tab_main: QWidget, keys: set, current_idx):
+    # define a new tabwidget then add this to main tabwidget's layout
     tabwid_child = QTabWidget(tab_main)
-
+    layout.addWidget(tabwid_child)
     # the tablist is the name of the tabs to be created
     for i in keys:
         tab_child = QWidget()
         tabwid_child.addTab(tab_child, i)
 
 
-    layout.addWidget(tabwid_child)
-
-    # then define a new layout for added tabwidget
-    # newtab_lay = QVBoxLayout()
-
-
-
-
-    if not tab_child.layout():
-        tabchild_lay = QGridLayout(tab_child)
-
 
     # set click signal for created tabs & set their layout
-    tabwid_child.currentChanged.connect(lambda: tabchanced(tabwid_child, jsonPy_global.jsondata["feeds"]))
-
-    # if not tabwid.currentWidget().layout():
-    #     tabwid.currentWidget().setLayout(newtab_lay)
-
+    tabwid_child.currentChanged.connect(lambda: tabchanced(tabwid_child, jsonPy_global.jsondata["feeds"][current_idx]))
+    tabchanced(tabwid_child, jsonPy_global.jsondata["feeds"][current_idx])
 
 def tabchanced(clickedtab: QTabWidget, data):
     # todo: ram usage for created tabs click
     # todo: define layout for tabwidget and then add to another one gridlayout as child
 
-    if not clickedtab.currentWidget().layout():
-        tabchild_lay = QGridLayout(clickedtab.currentWidget())
-
     plaintext = QPlainTextEdit(clickedtab.currentWidget())
-    # plaintext.setReadOnly(True)
-    plaintext.setPlainText(str(data))
+    plaintext.setReadOnly(True)
+    plaintext.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.ArrowCursor))
 
-    clickedtab.currentWidget().layout().addWidget(plaintext, 0, 0, 1, 1)
-    """
-    gridlayout = QGridLayout(clickedtab)
-    label = QLabel()
-    gridlayout.addWidget(label)
-    sizepolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-    sizepolicy.setVerticalPolicy(0)
-    sizepolicy.setHorizontalPolicy(0)
-    sizepolicy.setHeightForWidth(label.sizePolicy().hasHeightForWidth())
-    label.setSizePolicy(sizepolicy)
-    label.setText(str(data))
-    """
+    if isinstance(data[clickedtab.tabText(clickedtab.indexOf(clickedtab.currentWidget()))], dict):
+        plaintext.setPlainText("DİCT")
+    elif isinstance(data[clickedtab.tabText(clickedtab.indexOf(clickedtab.currentWidget()))], list):
+        plaintext.setPlainText("List")
+    else:
+        plaintext.setPlainText(str(data[clickedtab.tabText(clickedtab.indexOf(clickedtab.currentWidget()))]))
 
-    # print(clickedtab is jsonPy_global.current_clicked_up_tab)
-    # print(data)
+    # todo: when the 'addwidget' outside of this condition, it's creating new layouts
+    if not clickedtab.currentWidget().layout():
+        clickedtab.currentWidget().setLayout(QGridLayout(clickedtab.currentWidget()))
+        clickedtab.currentWidget().layout().addWidget(plaintext)
 
-    """
-        tablay = QVBoxLayout()
-        tablist = set()
-        print(tablist)
-        print(tablay)
-        print(clickedtab)
-        tabList.add(i)
-        if clickedTab.tabText(clickedTab.currentIndex())==str(i):
-            #functions.newTab(tabLay,i["name"])
-            list=set()
-            for j in i:
-                list.add(j)
-            print(list)
-            print(len(list))
-            newTab(tabLay,list)
-            break
 
-        #tabLay.addWidget(lbl)
-        #functions.newTab(tabLay)
-        clickedTab.currentWidget().setLayout(tabLay) 
-        """
