a2acd7b08070cb7b7fd9007096dcd173c74f80dd
Yasin KAYA <yasinkaya1007@gmail.com>
2021-09-06 20:45:59 +0300

    oop ui - py

--- UIs/GzIs_error_main.py
+++ UIs/GzIs_error_main.py
@@ -1,128 +1,58 @@
-from PIL import Image
-from PyQt5 import QtGui
-from PyQt5.QtCore import Qt, QCoreApplication, QSize
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QWidget, QGridLayout, QStatusBar, QPushButton, QCheckBox, QLabel, QComboBox, QHBoxLayout, \
-    QTableWidget, QVBoxLayout, QSizePolicy, QMenuBar, QMenu, QToolButton
+# -*- coding: utf-8 -*-
 
-import gzis_funcs, global_variables
-from classes import ComboBox, TabBar
+# Form implementation generated from reading ui file 'GzIs_error_main.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
 
 
-class UiMainWindow(object):
-    def __init__(self):
-        super(UiMainWindow, self).__init__()
-        self.centralwidget = QWidget()
-        self.statusbar = QStatusBar()
-        self.btn_palette = QPushButton()
-        self.check_exception = QCheckBox()
-        self.lbl_isaccept = QLabel()
-        self.cbx_isaccept = QComboBox()
-        self.cbx_ar_pos = ComboBox.MyComboBox()
-        self.cbx_jsons = ComboBox.MyComboBox()
-        self.layout_cbxes = QHBoxLayout()
-        self.table_content = QTableWidget()
-        self.tbar_headers = TabBar.CreateTabbar()
-        self.layout_content = QVBoxLayout()
-        self.menubar = QMenuBar()
-        self.menu = QMenu()
-        # self.x_btn = QToolButton()
-        self.main_layout = QGridLayout(self.centralwidget)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-    def setup_ui(self, main_window):
-        main_window.setObjectName("MainWindow")
-        main_window.setMinimumSize(QSize(1000, 500))
-        main_window.setMenuBar(self.menubar)
-        main_window.setStatusBar(self.statusbar)
-        main_window.setCentralWidget(self.centralwidget)
 
-        # self.x_btn.clicked.connect(main_window.close)
-        # main_window.menuBar().setCornerWidget(self.x_btn)
-
-        # Mainwindow Window icon
-        fname = r'icons/raptiye.png'
-        raptiye = Image.open(fname)
-        raptiye.save('icons/raptiye.ico', format='ICO', sizes=[(32, 32)])
-        main_window.setWindowIcon(QIcon('icons/raptiye.ico'))
-
-        # select test as arinc or posix then select the any json file
-        self.layout_cbxes.addWidget(self.cbx_ar_pos)
-        self.layout_cbxes.addWidget(self.cbx_jsons)
-
-        # this layout at main grid layout add tbar and tablewidget
-        self.layout_content.addWidget(self.tbar_headers)
-        self.layout_content.addWidget(self.table_content)
-
-        # then select a tab created dynamicly, its will create table dynamicly
-
-        self.main_layout.addLayout(self.layout_cbxes, 0, 0)
-        self.main_layout.addLayout(self.layout_content, 1, 0, Qt.AlignTop)
-
-        self.retranslate_ui(main_window)
-
-        self.init_statusbar()
-        self.init_comboboxes()
-
-        # main_window.setWindowFlag(Qt.FramelessWindowHint)
-        # self.init_custom_tilebar()
-
-    @staticmethod
-    def retranslate_ui(main_window):
-        _translate = QCoreApplication.translate
-        main_window.setWindowTitle(_translate("GzIS Error", "GzIS Error"))
-
-    def init_comboboxes(self):
-        self.cbx_jsons.setObjectName("cbx_paths")
-        self.statusbar.setObjectName("statusbar")
-        self.cbx_ar_pos.setObjectName("cbx_chooser")
-        self.main_layout.setObjectName("gridLayout")
-        self.tbar_headers.setObjectName("tabbar_headers")
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(738, 459)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-
-        with open("UIs/GzIs_cbx_style_sheet.css", "r") as cbxsheet:
-            self.cbx_isaccept.setStyleSheet(str(cbxsheet.read()))
-
-        self.cbx_ar_pos.sync_widget(global_variables.json_paths)
-        self.cbx_jsons.blockSignals(False)
-
-        size_policy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        size_policy.setHeightForWidth(self.cbx_ar_pos.sizePolicy().hasHeightForWidth())
-        size_policy.setHorizontalStretch(0)
-        size_policy.setVerticalStretch(0)
-        self.cbx_ar_pos.setSizePolicy(size_policy)
-
-    def init_statusbar(self):
-        self.statusbar.setStyleSheet("QStatusBar{"
-                                     "padding-left:8px;background: rgb(45,45,45);"
-                                     "color:#fffff0;font-weight;}")
-        self.btn_palette.setStyleSheet("QPushButton{background-color: rgb(45 ,45, 45); margin-right: 10px; }")
-        self.check_exception.setStyleSheet("QCheckBox{color: #fffff0; margin-right: 10px; margin-left: 10px;}")
-        self.lbl_isaccept.setStyleSheet("QLabel{color: #fffff0; margin-left: 10px}")
-        # Change Main window's palette with button at statusbar
-        icon = QtGui.QIcon()
-        icon.addPixmap(QPixmap('icons/palette.png'))
-        self.btn_palette.setIcon(QIcon('icons/palette.png'))
-
-        # when check_exception was clicked show other json (target.json)
-        self.check_exception.setText("Exception Test Suites: ")
-        self.check_exception.setLayoutDirection(Qt.RightToLeft)
-
-        # filter combobox using as tests' is accept value
-        self.lbl_isaccept.setText("Is Accept :")
-        self.cbx_isaccept.addItems(["All", "True", "False"])
-        # connect event - if is cbxisaccpet current text == false (or true)
-        # remove items which item's accept value of isaccept  is True (for isaccpet= false)
-        self.cbx_isaccept.currentIndexChanged.connect(lambda: gzis_funcs.isaccepted_filter(
-            self.table_content, self.cbx_isaccept.currentText()))
-
-        # add widgets to status bar
-        self.statusbar.addPermanentWidget(self.check_exception, 0)
-        self.statusbar.addPermanentWidget(self.btn_palette, 0)
-
-    def init_custom_tilebar(self):
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.layout_cbxes = QtWidgets.QHBoxLayout()
+        self.layout_cbxes.setObjectName("layout_cbxes")
+        self.cbx_ar_pos = QtWidgets.QComboBox(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.cbx_ar_pos.sizePolicy().hasHeightForWidth())
+        self.cbx_ar_pos.setSizePolicy(sizePolicy)
+        self.cbx_ar_pos.setStyleSheet("")
+        self.cbx_ar_pos.setObjectName("cbx_ar_pos")
+        self.layout_cbxes.addWidget(self.cbx_ar_pos)
+        self.cbx_jsons = QtWidgets.QComboBox(self.centralwidget)
+        self.cbx_jsons.setObjectName("cbx_jsons")
+        self.layout_cbxes.addWidget(self.cbx_jsons)
+        self.gridLayout.addLayout(self.layout_cbxes, 0, 1, 1, 1)
+        self.layout_content = QtWidgets.QVBoxLayout()
+        self.layout_content.setObjectName("layout_content")
+        self.table_content = QtWidgets.QTableWidget(self.centralwidget)
+        self.table_content.setObjectName("table_content")
+        self.table_content.setColumnCount(0)
+        self.table_content.setRowCount(0)
+        self.layout_content.addWidget(self.table_content)
+        self.gridLayout.addLayout(self.layout_content, 1, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 738, 24))
         self.menubar.setObjectName("menubar")
-        self.menu.setTitle("kak")
-        self.menu.addAction("kek")
-        self.menu.setObjectName("menu")
-        self.menubar.addAction(self.menu.menuAction())
-        self.x_btn.setText("X")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
--- classes/Main_Window.py
+++ classes/Main_Window.py
@@ -1,29 +1,30 @@
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtGui import QPalette, QColor
-from UIs import GzIs_error_main
+from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QSizePolicy, QPushButton, QCheckBox, QLabel, QComboBox
+from PyQt5.QtGui import QPalette, QColor, QPixmap, QIcon
 import global_variables, gzis_funcs
-from classes import table_widget_tests, table_widget_target
+from classes import table_widget_tests, table_widget_target, TabBar
+from UIs.GzIs_error_main import Ui_MainWindow
 
 
-class Window(QMainWindow):
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(Window, self).__init__()
-        main_ui = GzIs_error_main.UiMainWindow()
-        main_ui.setup_ui(self)
 
-        self.cbx_jsons = main_ui.cbx_jsons
-        self.statusbar = main_ui.statusbar
-        self.cbx_ar_pos = main_ui.cbx_ar_pos
-        self.btn_palette = main_ui.btn_palette
-        self.cbx_isaccept = main_ui.cbx_isaccept
-        self.tbar_headers = main_ui.tbar_headers
-        self.lbl_isaccept = main_ui.lbl_isaccept
-        self.table_content = main_ui.table_content
-        self.check_exception = main_ui.check_exception
+        self.btn_palette = QPushButton()
+        self.check_exception = QCheckBox()
+        self.lbl_isaccept = QLabel()
+        self.cbx_isaccept = QComboBox()
 
+        self.tbar_headers = TabBar.CreateTabbar()
+
+        self.setupUi(self)
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
+        self.layout_content.addWidget(self.tbar_headers)
         self.setup_widget()
-
-
+        self.init_comboboxes()
+        self.init_statusbar()
     def setup_widget(self):
         self.btn_palette.clicked.connect(self.btn_palette_trigger)
         self.check_exception.clicked.connect(self.check_target_trigger)
@@ -36,7 +37,8 @@ class Window(QMainWindow):
         self.clear_contents()
         self.tbar_headers.blockSignals(False)
         gzis_funcs.sync_global_jsonfiles(self.cbx_ar_pos)
-        self.cbx_jsons.sync_widget(global_variables.json_files)
+        self.cbx_jsons.clear()
+        self.cbx_jsons.addItems(global_variables.json_files)
 
     def cbx_paths_current_changed(self):
         if self.cbx_jsons.currentIndex() != -1:
@@ -98,3 +100,52 @@ class Window(QMainWindow):
     def set_table_header_visible(self, val: bool):
         self.table_content.verticalHeader().setVisible(val)
         self.table_content.horizontalHeader().setVisible(val)
+
+# ------------------------------------------------------------------
+    def init_comboboxes(self):
+        self.cbx_jsons.setObjectName("cbx_paths")
+        self.statusbar.setObjectName("statusbar")
+        self.cbx_ar_pos.setObjectName("cbx_chooser")
+
+        self.tbar_headers.setObjectName("tabbar_headers")
+        self.centralwidget.setObjectName("centralwidget")
+
+        with open("UIs/GzIs_cbx_style_sheet.css", "r") as cbxsheet:
+            self.cbx_isaccept.setStyleSheet(str(cbxsheet.read()))
+
+        self.cbx_ar_pos.addItems(global_variables.json_paths)
+        self.cbx_jsons.blockSignals(False)
+
+        size_policy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        size_policy.setHeightForWidth(self.cbx_ar_pos.sizePolicy().hasHeightForWidth())
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(0)
+        self.cbx_ar_pos.setSizePolicy(size_policy)
+
+    def init_statusbar(self):
+        self.statusbar.setStyleSheet("QStatusBar{"
+                                     "padding-left:8px;background: rgb(45,45,45);"
+                                     "color:#fffff0;font-weight;}")
+        self.btn_palette.setStyleSheet("QPushButton{background-color: rgb(45 ,45, 45); margin-right: 10px; }")
+        self.check_exception.setStyleSheet("QCheckBox{color: #fffff0; margin-right: 10px; margin-left: 10px;}")
+        self.lbl_isaccept.setStyleSheet("QLabel{color: #fffff0; margin-left: 10px}")
+        # Change Main window's palette with button at statusbar
+        icon = QtGui.QIcon()
+        icon.addPixmap(QPixmap('icons/palette.png'))
+        self.btn_palette.setIcon(QIcon('icons/palette.png'))
+
+        # when check_exception was clicked show other json (target.json)
+        self.check_exception.setText("Exception Test Suites: ")
+        self.check_exception.setLayoutDirection(Qt.RightToLeft)
+
+        # filter combobox using as tests' is accept value
+        self.lbl_isaccept.setText("Is Accept :")
+        self.cbx_isaccept.addItems(["All", "True", "False"])
+        # connect event - if is cbxisaccpet current text == false (or true)
+        # remove items which item's accept value of isaccept  is True (for isaccpet= false)
+        self.cbx_isaccept.currentIndexChanged.connect(lambda: gzis_funcs.isaccepted_filter(
+            self.table_content, self.cbx_isaccept.currentText()))
+
+        # add widgets to status bar
+        self.statusbar.addPermanentWidget(self.check_exception, 0)
+        self.statusbar.addPermanentWidget(self.btn_palette, 0)
